d2c53e8b58b3af244c98f12638cc207cf7de74c7
manibharathis <remasekar.96@gmail.com>
2025-02-24 13:02:24 +0530

    this inside object

--- type.js
+++ type.js
@@ -1,1 +1,17 @@
-console.log(2 + '2'-1);
+//console.log(2 + '2'-1);
+
+//console.log([] == ![]);
+
+//console.log(3 < 2 < 1);
+
+const object1 = {
+    a: 10,
+    b: 20,
+    c: function () {
+      console.log(this.a + this.b);
+    },
+  };
+  const result = object1.c;
+  console.log(result)
+  //
+  // result.c();
